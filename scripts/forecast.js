3a09874809c1dcd7b24d05f262f74b86cdf5590d
Victor Bondarenko <nirasimha@gmail.com>
2022-04-06 17:54:59 +0000

    add getWeather func and return Key to get weather

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -11,6 +11,21 @@ const getCity = async (city) => {
 
 };
 
+const getWeather = async (id) => {
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  // console.log(data);
+  return data[0];
+};
+
 getCity('toronto')
+  // .then(data => console.log(data))
+  .then(data => {
+    return getWeather(data.Key)
+  })
   .then(data => console.log(data))
   .catch(err => console.log(err));
