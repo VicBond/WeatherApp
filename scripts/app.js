a69a04caa7aa6f66b62684380cb571884e4a0943
Victor Bondarenko <nirasimha@gmail.com>
2022-04-07 17:57:43 +0000

    add update city func

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,15 @@
 const cityForm = document.querySelector('form');
 
+const updateCity = async () => {
+  console.log(city);
+};
+
 cityForm.addEventListener('submit', e => {
   e.preventDefault();//prevent default event
   
-  const getCity = cityForm.city.value.trim();
+  const getCity = cityForm.city.value.trim(); //get city value
 
   cityForm.reset();
-})
+
+  updateCity(city);
+});
