cc20d256ccfd7a131a1e80f1db0c75bcc56c7a6f
Victor Bondarenko <nirasimha@gmail.com>
2022-04-07 17:54:17 +0000

    add event listener to city form submit

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,9 @@
 const cityForm = document.querySelector('form');
 
+cityForm.addEventListener('submit', e => {
+  e.preventDefault();//prevent default event
+  
+  const getCity = cityForm.city.value.trim();
+
+  cityForm.reset();
+})
